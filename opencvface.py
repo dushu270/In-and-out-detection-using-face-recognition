1b2e8a53210bcbfd26e08738058d73ecce705de4
Dushyanth Sai kiran <dushu270@gmail.com>
2022-03-17 23:14:52 +0530

    working with faces gone and came

--- opencvface.py
+++ opencvface.py
@@ -4,14 +4,16 @@ import numpy as np
 import cv2
 import pickle
 import datetime
+import time
 
 
 left, center, right = False, False, False
 x=150
-
+mask = np.zeros((1000,1000))
 face_cascade=cv2.CascadeClassifier('test1/cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer=cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("test1/trainer.yml")
+ytest_pos = 40
 
 labels={}
 with open("test1/labels.pickle","rb") as f:
@@ -52,11 +54,14 @@ while(True):
     
     ret,frame=cap.read()
     frame=rescale_frame(frame,percent=80)
+    frame=cv2.flip(frame,1)
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=face_cascade.detectMultiScale(image=gray,scaleFactor=1.5,minNeighbors=5)
 
     for(x1,y1,w1,h1) in faces:
+
         #print(x,y,w,h)
+        x=x1
         roi_gray=gray[y1:y1+h1,x1:x1+w1]
         roi_color=frame[y1:y1+h1,x1:x1+w1]
         
@@ -90,8 +95,12 @@ while(True):
             elif x > 300:
                 if center:
                     print("motion to right taken place")
-                    mask = np.zeros((200, 400))
-                    cv2.putText(mask, "to right", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255), 3)
+                    '''outp = np.zeros((1920, 500,300))
+                    cv2.putText(outp, ("gone at {}".format(datetime.datetime.now().strftime("%H:%M"))+name),(0,0),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255))'''
+                    #mask = np.zeros((1000, 1000))
+                    ytest_pos+=50
+                    cv2.putText(mask, ("gone at {}".format(datetime.datetime.now().strftime("%H:%M"))+name), (50, ytest_pos), cv2.FONT_HERSHEY_SIMPLEX, 2, (255), 3)
                     left = False
                     center = False
                 else:
@@ -104,8 +113,14 @@ while(True):
             if x < 100:
                 if center:
                     print("motion done to the left side")
-                    mask = np.zeros((200, 400))
-                    cv2.putText(mask, "to left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255), 3)
+                    ''' outp = np.zeros((1920,500, 300))
+                    cv2.putText(outp, ("came at {}".format(datetime.datetime.now().strftime("%H:%M"))+name),(0,0),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255))'''
+
+                    #mask = np.zeros((1920, 500,300))
+                    #mask = np.zeros((1000, 1000))
+                    ytest_pos+=50
+                    cv2.putText(mask, ("came at {}".format(datetime.datetime.now().strftime("%H:%M"))+name), (50, ytest_pos), cv2.FONT_HERSHEY_SIMPLEX, 2, (255), 3)
                     right = False
                     center = False
 
@@ -124,7 +139,8 @@ while(True):
         width=x1+w1
         height=y1+h1
         cv2.rectangle(frame, (x1,y1),(width,height),color,stroke) 
-    cv2.imshow('frame',frame)   
+    cv2.imshow('frame',frame)
+    cv2.imshow("mask", mask)   
     if(cv2.waitKey(20) & 0xFF ==  ord('q')):
         break    
     
